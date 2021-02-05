40433e7711d432112158bcbeecfa3e27b44d2a0c
petrovskyd <petrovsky.dv@gmail.com>
2021-02-05 14:56:08 +0300

    добавил README.md, добавил LICENSE, удалил закомментированный код

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # logger.setLevel(logging.DEBUG)
 
     load_dotenv()
     vk_access_token = os.getenv('VK_ACCESS_TOKEN')
